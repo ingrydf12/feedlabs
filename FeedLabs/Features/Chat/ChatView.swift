f86a6942b92aac4e7a60d8092df4613e4e7b1fbe
jpvinhas <joaopedrovinhasborges@gmail.com>
2024-07-29 10:44:37 -0300

    "feat: chat group"

---
+++ FeedLabs/Features/Chat/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  FeedLabs
+//
+//  Created by João Pedro Borges on 29/07/24.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text("Chat")
+    }
+}
+
+#Preview {
+    ChatView()
+}
