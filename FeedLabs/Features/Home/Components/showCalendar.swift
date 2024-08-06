bc15b8870c0faddbf286a7c234b705febc6edbd9
Guilherme Pessoa Marinho <pessoamarine09@gmail.com>
2024-08-06 08:51:31 -0300

    atualizacao

---
+++ FeedLabs/Features/Home/Components/showCalendar.swift
@@ -0,0 +1,18 @@
+//
+//  showCalendar.swift
+//  FeedLabs
+//
+//  Created by Guilherme Pessoa on 31/07/24.
+//
+
+import SwiftUI
+
+struct showCalendar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    showCalendar()
+}
