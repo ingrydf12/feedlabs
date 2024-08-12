f6fd1264181df27b8830934096ea03a669be9819
Ingryd <ingrydcduarte@gmail.com>
2024-08-12 10:21:59 -0300

    add: complete visual event card

--- FeedLabs/Features/Home/Components/EventCard.swift
+++ FeedLabs/Features/Home/Components/EventCard.swift
@@ -10,80 +10,94 @@ struct EventCard: View {
 
     private static let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
-        formatter.dateFormat = "MMMM dd, yyyy"
+        formatter.dateFormat = "MMMM dd"
         return formatter
     }()
     
     var body: some View {
         ScrollView(.vertical) {
-            ForEach(eventManager.events ?? []) { event in
+            ForEach(eventManager.events) { event in
+                //MARK: - Event Details
                 HStack(spacing: 30) {
+                    //Título do evento
                     VStack(alignment: .leading) {
                         if let name = event.name {
                             Text(name)
                             //Change font to Tahoma 32 (title)
-                                .font(.system(size: 32, weight: .bold))
+                                .font(.tahoma(.bold, size: 32))
                         }
                         
+                        //Creator
+                        if let firstOwnerId = event.owners.first,
+                           let owner = UserManager.shared.getUserById(firstOwnerId) { // Obtém o dono correspondente ao ID
+                            Text(owner.name ?? "") // Exibe o nome do dono
+                                .font(.tahoma(.regular, size: 12))
+                                .multilineTextAlignment(.center)
+                        }
+                        
+                        //Texto bonito lá pra participantes do evento
                         HStack {
                             if let firstParticipantId = event.participants.first,
                                let firstParticipant = UserManager.shared.getUserById(firstParticipantId) {
-                                Text(firstParticipant.name ?? "")
-                                    .font(.system(size: 10, weight: .light))
-                                    .multilineTextAlignment(.center)
                                 
-                                if event.participants.count > 1 {
-                                    Text("+\(event.participants.count - 1)")
-                                        .font(.system(size: 10, weight: .light))
-                                        .multilineTextAlignment(.center)
-                                }
+                                let remainingParticipantsCount = event.participants.count - 1
+                                
+                                Text("\(firstParticipant.name ?? "") e mais \(remainingParticipantsCount) pessoa\(remainingParticipantsCount > 1 ? "s" : "")")
+                                    .font(.tahoma(.regular, size: 14))
+                                    .multilineTextAlignment(.leading)
                             }
                         }
 
+
                     }
+                    
+                    
                     .padding()
                     
                     VStack(alignment: .trailing ,spacing: 15) {
                         //TAG EVENT: EventType
                         Text(event.type.rawValue)
-                            .font(.caption)
+                            .font(.tahoma(.regular, size: 16))
                             .padding(5)
-                            .foregroundStyle(Color.darkAqua) //Change name to "Primary Color"" or Action
+                            .foregroundStyle(Color.black) //Change name to "Inactive color" or Black
                             .background(
-                                RoundedRectangle(cornerRadius: 15)
+                                RoundedRectangle(cornerRadius: 5)
                                     .stroke(Color.gray.opacity(0.5), lineWidth: 2)
                             )
 
-                        //Must to appear when (Team Details)
+                        //MARK: - Must to appear when (Team Details)
                         //Image(systemName: "plus.app")
                         
-                        HStack {
-                            if event.owners.contains(user) {
-                                Button {
-                                    selectedEvent = event.id ?? ""
-                                    showingInviteModal.toggle()
-                                } label: {
-                                    Image(systemName: "person.badge.plus")
-                                        .foregroundColor(.darkAqua)
-                                }
-                                Button {
-                                    if let id = event.id {
-                                        eventManager.deleteEvent(id)
-                                    }
-                                } label: {
-                                    Image(systemName: "trash.fill")
-                                        .foregroundColor(.red)
-                                }
-                            }
-                        }
+//                        HStack {
+//                            if event.owners.contains(user) {
+//                                Button {
+//                                    selectedEvent = event.id ?? ""
+//                                    showingInviteModal.toggle()
+//                                } label: {
+//                                    Image(systemName: "person.badge.plus")
+//                                        .foregroundColor(.darkAqua)
+//                                }
+//                                Button {
+//                                    if let id = event.id {
+//                                        eventManager.deleteEvent(id)
+//                                    }
+//                                } label: {
+//                                    Image(systemName: "trash.fill")
+//                                        .foregroundColor(.red)
+//                                }
+//                            }
+//                        }
                         
-                        //TAG EVENT: Date Formatter
-                        Text(Self.dateFormatter.string(from: event.date ?? Date()))
-                            .font(.caption)
-                            .padding(5)
-                            .foregroundStyle(Color.blue) //Change name to "Inactive color
-                            .background(Color.gray.opacity(0.2))
-                            .cornerRadius(10)
+                        //MARK: - TAG EVENT: Date Formatter
+                        HStack (alignment: .center){
+
+                            Image(systemName: "calendar")
+                                .foregroundStyle(Color.black)
+                            Text(Self.dateFormatter.string(from: event.date ?? Date()))
+                                .font(.tahoma(.regular, size: 16))
+                                .foregroundStyle(Color.black)
+                                .cornerRadius(10)
+                        }
                     }
                 }
                 .padding()
