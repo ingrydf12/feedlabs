8140c2437191faaf96d24a426d3277697bec2d52
Pedro Furtado <pedrofurtado1.9@gmaill.com>
2024-08-19 11:48:07 -0300

    Connect chat with invitesView

--- FeedLabs/Features/Chats/ChatList.swift
+++ FeedLabs/Features/Chats/ChatList.swift
@@ -26,6 +26,13 @@ struct ChatList: View {
                             .foregroundColor(.gray)
                     })
                 }
+                HStack{
+                    NavigationLink(destination: InvitesView()){
+                        Image(systemName: "exclamationmark.bubble.circle.fill")
+                            .font(.largeTitle).foregroundColor(Color("darkAqua") )
+                        Text("Convites")
+                    }
+                }
                 if !chatManager.filteredUsersByChats.isEmpty {  // !
                     if !(chatManager.searchText == "") &&   chatManager.isSearchingUser {
                         Section(header: Text("Selecionar conversa")){
